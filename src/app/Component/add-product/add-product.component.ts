cdc967a2b9746b96207a593183907ec7edbc93d8
Heba_El-Sayed <hebaelsayedcs@gmail.com>
2024-03-28 04:27:00 +0200

    sort by price and category added

--- src/app/Component/add-product/add-product.component.ts
+++ src/app/Component/add-product/add-product.component.ts
@@ -4,12 +4,11 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
-  styleUrl: './add-product.component.css'
+  styleUrl: './add-product.component.css',
 })
 export class AddProductComponent {
-@ViewChild('addForm' , {static: true}) AddProductForm !: NgForm ;
-  AddProduct(){
+  @ViewChild('addForm', { static: true }) AddProductForm!: NgForm;
+  AddProduct() {
     console.log(this.AddProductForm);
-    
   }
 }
